c67e12018dfc69ff31d40c5020262d22bf921cab
Luis Mendez <dluismendezdj@gmail.com>
2022-07-05 15:17:07 -0400

    feat: add firebase config

--- src/globalValues.js
+++ src/globalValues.js
@@ -1,3 +1,30 @@
+// Global values for entire application
+
+// Calendar menu items
 const CALENDAR_ITEMS = ["today", "next 7 days", "all days"];
 
-export { CALENDAR_ITEMS };
+// Firebase values configuration
+const FIREBASE_API_KEY = "AIzaSyBlIURB2kUPUTw4STE6eqrGMP0MBBYvYHs";
+const FIREBASE_AUTH_DOMAIN = "todo-firebase-451eb.firebaseapp.com";
+const FIREBASE_PROJECT_ID = "todo-firebase-451eb";
+const FIREBASE_STORAGE_BUCKET = "todo-firebase-451eb.appspot.com";
+const FIREBASE_MESSAGING_SENDER_ID = "49842254315";
+const FIREBASE_APP_ID = "1:49842254315:web:031106b44afa64d44fa9ae";
+const FIREBASE_MEASUREMENT_ID = "G-NCHMF17HT2";
+
+// Firebase collections
+const FIREBASE_TODOS_COLLECTION_NAME = "todos";
+const FIREBASE_PROJECTS_COLLECTION_NAME = "projects";
+
+export {
+	CALENDAR_ITEMS,
+	FIREBASE_API_KEY,
+	FIREBASE_AUTH_DOMAIN,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_STORAGE_BUCKET,
+	FIREBASE_MESSAGING_SENDER_ID,
+	FIREBASE_APP_ID,
+	FIREBASE_MEASUREMENT_ID,
+	FIREBASE_TODOS_COLLECTION_NAME,
+	FIREBASE_PROJECTS_COLLECTION_NAME,
+};
---
+++ src/helpers/firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+	FIREBASE_API_KEY,
+	FIREBASE_AUTH_DOMAIN,
+	FIREBASE_PROJECT_ID,
+	FIREBASE_STORAGE_BUCKET,
+	FIREBASE_MESSAGING_SENDER_ID,
+	FIREBASE_APP_ID,
+	FIREBASE_MEASUREMENT_ID,
+} from "../globalValues";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase configuration
+const firebaseConfig = {
+	apiKey: FIREBASE_API_KEY,
+	authDomain: FIREBASE_AUTH_DOMAIN,
+	projectId: FIREBASE_PROJECT_ID,
+	storageBucket: FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+	appId: FIREBASE_APP_ID,
+	measurementId: FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
