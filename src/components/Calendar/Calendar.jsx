288910a114e6f728074878d5ddd9b64ad7a64bf1
Luis Mendez <dluismendezdj@gmail.com>
2022-07-04 23:27:33 -0400

    feat: style calendar component

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,3 +1,29 @@
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import { CALENDAR_ITEMS } from "../../globalValues";
+import "./Calendar.css";
+
 export default function Calendar() {
-	return <div>Calendar</div>;
+	return (
+		<div className="calendar">
+			<div className="header">
+				<div className="title">
+					<CalendarMonthOutlinedIcon size="18" />
+					<p>Calendar</p>
+				</div>
+				<div className="btns">
+					<span>
+						<KeyboardArrowUpOutlinedIcon size="20" />
+					</span>
+				</div>
+			</div>
+			<div className="items">
+				{CALENDAR_ITEMS.map((item) => (
+					<div className="item" key={item}>
+						{item}
+					</div>
+				))}
+			</div>
+		</div>
+	);
 }
